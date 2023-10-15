f45ce1583f1e79f81ad22078bb55baed8aa65e96
ennypian adekoya <ennypitan@gmail.com>
2023-10-16 00:12:22 +0100

    updated

--- App.js
+++ App.js
@@ -1,54 +1,34 @@
 // AOS
 AOS.init({
-    duration: 1200,
-  })
+  duration: 1200,
+});
 
-  
-  var splide = new Splide( '#first-slider', {
-    type    : 'loop',
-    perPage : 3.5,
-    autoplay: true,
-    perMove: 1,
-    // padding: '5rem',
-    wheel    : true,
-    gap:'1.5rem',
-    breakpoints: {
-      760: {
-        perPage: 3,
-      },
-    },
-    breakpoints: {
-      480: {
-        perPage: 1,
-      },
-    }
-  } );
-  
-  splide.mount();
-
-  var splide = new Splide( '#second-slider', {
-    type    : 'loop',
-    perPage : 4,
-    autoplay: true,
-    perMove: 1,
-    arrows:  false,
-    pagination: false,
-    height: '100px',
-    // padding: '5rem',
-    wheel    : true,
-    interval: number = 2000
-   
-  } );
-  
-  splide.mount();
-
-   
 //hamburger
 
-const hamburger = document.querySelector('.responsive-menu-icon');
-const mobileMenu = document.querySelector('.menu-overlay');
+const hamburger = document.querySelector(".responsive-menu-icon");
+const mobileMenu = document.querySelector(".menu-overlay");
 
-hamburger.addEventListener('click', ()=>{
-  hamburger.classList.toggle('active')
-  mobileMenu.classList.toggle('active')
-})
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+});
+
+// gallery.js
+const images = document.querySelectorAll(".image-container img");
+const lightbox = document.createElement("div");
+lightbox.classList.add("lightbox");
+
+images.forEach((image, index) => {
+  console.log("it is working");
+  image.addEventListener("click", () => {
+    lightbox.innerHTML = `<img src="${image.src}" alt="Image ${index + 1}" />`;
+    lightbox.style.display = "block";
+    console.log("it is working");
+  });
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.style.display = "none";
+});
+document.querySelector(".service-content").appendChild(lightbox);
